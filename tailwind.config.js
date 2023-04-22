c32de52570044e6f89d1ca74ae4fd4d3ade1c9f7
Diana Villarreal <dianacarolinav97@gmail.com>
2023-04-21 19:46:27 -0500

    ci: tailwind_config_colors_and_fontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,18 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   // Ensure these match with .storybook/preview.js
+
   theme: {
     extend: {
       colors: {
-        'my-custom-color': '#fffff',
+        white: '#FFFFFF',
+        'blue-sky': '#E6ECF5',
+        red: '#EF3061',
+        'blue-dark': '#07469C',
+        dark: '#333333',
+        'blue-light': '#0099E1',
+        'gray-light': '#757575',
+        'white-dark': '#F2F2F2',
       },
       screens: {
         xs: '375px',
@@ -16,6 +24,10 @@ module.exports = {
         md: '900px',
         lg: '1200px',
         xl: '1536px',
+      },
+      fontFamily: {
+        nunito: ['Nunito'],
+        mulish: ['Mulish'],
       },
     },
     plugins: [],
