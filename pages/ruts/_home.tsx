94affa34dd601147fc802a64a2d9dc09adb79f2e
Diana Villarreal <dianacarolinav97@gmail.com>
2023-04-23 11:02:11 -0500

    feat: acomoding-space-work

--- pages/ruts/_home.tsx
+++ pages/ruts/_home.tsx
@@ -1,22 +1,23 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
 function Home() {
   return (
-    <ul>
-      <li>
-        <Link href="/">Home</Link>
-      </li>
-      <li>
-        <Link href="/ruts/_auth" >Ahth</Link>
-      </li>
-      <li>
-        <Link href="/ruts/_home-application">Application</Link>
-      </li>
-    </ul>
-  )
+    <>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/ruts/_auth">Ahth</Link>
+        </li>
+        <li>
+          <Link href="/ruts/_home-application">Application</Link>
+        </li>
+      </ul>
+    </>
+  );
 }
 
-export default Home
-
+export default Home;
 
 // pruebas de steven
