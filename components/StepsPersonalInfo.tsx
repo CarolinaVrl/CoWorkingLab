6e935fceda60cf19673e821ae75d0ca63f6ed8c1
Diana Villarreal <dianacarolinav97@gmail.com>
2023-05-15 18:46:43 -0500

    fix: application

--- components/StepsPersonalInfo.tsx
+++ components/StepsPersonalInfo.tsx
@@ -6,8 +6,8 @@ import ModalHelp from './ModalHelp';
 import TextCardHome from './Text-card-home';
 function StepsPersonalInfo() {
   const [isVisible, setIsVisible] = useState(false);
-  const showData = (values: any) => {
-    alert(values);
+  const showData = () => {
+    <Link href="/application"></Link>;
   };
   const closeModal = () => {
     setIsVisible(false);
@@ -15,10 +15,10 @@ function StepsPersonalInfo() {
 
   return (
     <div
-      className="flex items-center justify-center md:flex content-center lg:px-[150px] items-center justify-center
+      className="flex items-center justify-center md:flex  px-[150px] items-center justify-center 
     "
     >
-      <div className="flex flex-col items-center justify-center gap-10 py-[60px]">
+      <div className="flex flex-col items-center justify-center text-left gap-10 py-[60px]">
         <section className="px-3.5 flex flex-col gap-4">
           <Link
             href="/"
@@ -33,7 +33,7 @@ function StepsPersonalInfo() {
           </div>
         </section>
 
-        <section>
+        <section className="pb-10 shadow-lg">
           <Formik
             initialValues={{
               name: '',
@@ -50,43 +50,49 @@ function StepsPersonalInfo() {
             }}
             onSubmit={showData}
           >
-            <Form className="rounded-[10px] w-[391px] shadow-lg  font-mulish  flex flex-col px-[17px] gap-10 pb-16 items-center text-left">
+            <Form className="rounded-[10px] w-[391px] shadow-lg  font-mulish  flex flex-col px-[17px] gap-10 pb-16 items-center text-left lg: w-[665px] h-[679px] ">
               <Field
                 className=" font-mulish w-full px-7 py-1 border-b-black border-b-solid border-2                outline-none focus:border-b-blueLight peer placeholder-black"
                 name="name"
                 placeholder="Legal Name"
                 type="text"
               />
-              <Field
-                className=" font-mulish w-full px-7 py-1 border-b-black border-b-solid border-2                outline-none focus:border-b-blueLight peer placeholder-black"
-                name="nacionality"
-                placeholder="Your Nacionality"
-                type="text"
-              />
-              <Field
-                className=" font-mulish w-full px-7 py-1 border-b-black border-b-solid border-2                outline-none focus:border-b-blueLight peer placeholder-black"
-                name="gender"
-                placeholder="Your Gender"
-                type="text"
-              />
+              <div className="flex flex-col md:flex-row gap-[10px] w-full">
+                <Field
+                  className=" font-mulish w-full px-7 py-1 border-b-black border-b-solid border-2                outline-none focus:border-b-blueLight peer placeholder-black"
+                  name="nacionality"
+                  placeholder="Your Nacionality"
+                  type="text"
+                />
+                <Field
+                  className=" font-mulish w-full px-7 py-1 border-b-black border-b-solid border-2                outline-none focus:border-b-blueLight peer placeholder-black"
+                  name="gender"
+                  placeholder="Your Gender"
+                  type="text"
+                />
+              </div>
+
               <Field
                 className=" font-mulish w-full px-7 py-1 border-b-black border-b-solid border-2                outline-none focus:border-b-blueLight peer placeholder-black"
                 name="residence"
                 placeholder="Your Residence"
                 type="text"
               />
-              <Field
-                className=" font-mulish w-full px-7 py-1 border-b-black border-b-solid border-2                outline-none focus:border-b-blueLight peer placeholder-black"
-                name="addres"
-                placeholder="Residence Addres"
-                type="text"
-              />
-              <Field
-                className="font-mulish w-full px-7 py-1 border-b-black border-b-solid border-2                outline-none focus:border-b-blueLight peer placeholder-black"
-                name="postcode"
-                placeholder="Postcode"
-                type="text"
-              />
+              <div className="flex flex-col md:flex-row gap-[10px] w-full">
+                <Field
+                  className=" font-mulish w-full px-7 py-1 border-b-black border-b-solid border-2                outline-none focus:border-b-blueLight peer placeholder-black "
+                  name="addres"
+                  placeholder="Residence Addres"
+                  type="text"
+                />
+                <Field
+                  className="font-mulish w-full px-7 py-1 border-b-black border-b-solid border-2                outline-none focus:border-b-blueLight peer placeholder-black md:w-3/5"
+                  name="postcode"
+                  placeholder="Postcode"
+                  type="text"
+                />
+              </div>
+
               <Field
                 className="font-mulish w-full px-7 py-1 border-b-black border-b-solid border-2                outline-none focus:border-b-blueLight peer placeholder-black"
                 name="phone"
@@ -117,12 +123,17 @@ function StepsPersonalInfo() {
                 placeholder="Any comments"
                 type="text"
               />
-              <button
+              <Link
                 className="text-white w-full text-sm h-[55.92px] rounded-[10px] bg-blueLight text-mulish font-bold"
-                type="submit"
+                href="/application"
               >
-                SAVE AND CONTINUE
-              </button>
+                <button
+                  className="text-white w-full text-sm h-[55.92px] rounded-[10px] bg-blueLight text-mulish font-bold"
+                  type="submit"
+                >
+                  SAVE AND CONTINUE
+                </button>
+              </Link>
             </Form>
           </Formik>
         </section>
