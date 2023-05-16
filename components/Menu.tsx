21bfc1ce90c3bfd73259d2ae1a38e961191afa44
StevenSKLs <davinchisonico@gmail.com>
2023-05-15 23:35:10 -0500

    docs: menu_

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,11 +1,3 @@
-import {
-  Avatar,
-  Menu,
-  MenuHandler,
-  MenuItem,
-  MenuList,
-  Typography,
-} from '@material-tailwind/react';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -13,79 +5,26 @@ const Menu_ruts = () => {
   return (
     <>
       <style jsx>{
-        `.menu_provisional{
+        `.menuHome{
           text-align: center;
-          position: absolute;
           top: 13px;
           left: 8px;
           z-index: 2;
             }`
       }
       </style>
+      <Link href="/">
+        <div className="absolute flex menuHome">
+          <h2 className="text-3xl  px-5 font-bold font-nunito">resimex</h2>
+          <Image
+            alt=""
+            width={35}
+            height={35}
+            src={'/imgs_users/flag_Mexico.png'}
+          />
+        </div>
+      </Link>
 
-      <div className="menu_provisional">
-        <Menu placement="top-start">
-          <MenuHandler>
-            <div className="flex">
-              <h2 className="text-3xl  px-5 font-bold font-nunito">resimex</h2>
-              <Image
-                alt=""
-                width={35}
-                height={35}
-                src={'/imgs_users/flag_Mexico.png'}
-              />
-            </div>
-          </MenuHandler>
-          <MenuList>
-            <Link href="/">
-              <MenuItem className="flex items-center gap-2">
-                <Avatar
-                  variant="circular"
-                  alt="candice wu"
-                  className="h-4 w-4"
-                  src="/developer_full_stack/user_1.jpg"
-                />
-                <Typography variant="small" className="font-normal">
-                  Home
-                </Typography>
-              </MenuItem>
-            </Link>
-            <Link href="/auth/sing-up">
-              <MenuItem className="flex items-center gap-2">
-                <Avatar
-                  variant="circular"
-                  alt="candice wu"
-                  className="h-4 w-4"
-                  src="/developer_full_stack/user_5.jpg"
-                />
-                <Typography variant="small" className="font-normal">
-                  Auth
-                </Typography>
-              </MenuItem>
-            </Link>
-            <hr className="my-2 border-blue-gray-50" />
-            <Link href="/application">
-              <MenuItem className="flex items-center gap-2">
-                <Avatar
-                  variant="circular"
-                  alt="candice wu"
-                  className="h-4 w-4"
-                  src="/developer_full_stack/user_2.jpg"
-                />
-                <Avatar
-                  variant="circular"
-                  alt="candice wu"
-                  className="h-4 w-4"
-                  src="/developer_full_stack/user_4.jpg"
-                />
-                <Typography variant="small" className="font-normal">
-                  Application
-                </Typography>
-              </MenuItem>
-            </Link>
-          </MenuList>
-        </Menu>
-      </div>
     </>
   );
 };
--- pages/_home.tsx
+++ pages/_home.tsx
@@ -4,12 +4,9 @@ import SeamlessService from '@/components/Seamless-Service';
 import SenMessageHome from '@/components/SenMessageHome';
 import ValuableInvest from '@/components/Valuable-Invest';
 import WhyChoose from '@/components/Why-Choose';
-import Ruts_menu from '../components/Menu';
-
 function Home() {
   return (
     <>
-      <Ruts_menu />
       <LayoutHome>
         <h1 className="font-semibold text-nunito text-center text-[41px] py-[141px]">
           Lets go it together!
